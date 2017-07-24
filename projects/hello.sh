210a95e8cd6104b24dc8e2d79cde48e6ec0301d6
Marchete <marchena.carlos@gmail.com>
2017-07-24 13:33:11 -0500

    Testing

--- projects/hello.sh
+++ projects/hello.sh
@@ -1,19 +1,22 @@
-# { autofold
 #!/bin/bash
-
 VIEWERFILE="/project/target/content.html"
 TESTING="echo \"Hello World!\""
-history -a
-history -c
-history -r
+echo "Veo Historias"
+# history -a
+# history -c
+# history -r
+
+
 function checkHistory {
+echo "funcion"
   COUNT=0
   echo "<h1>Please write the following command: echo \"Hello World!\" $COUNT </h1></p>`history`">$VIEWERFILE
   FINISHED=0
   while [ $FINISHED -eq 0 ]; do
-    FILE=`history | grep "$1" | grep -v history |wc -l`
+    FILE="hola"
+	#`history | grep "$1" | grep -v history |wc -l`
     echo "Status of $FILE ">/tmp/status
-    history >/tmp/history 
+    # history >/tmp/history 
     if [ $FILE -ge 1 ]
     then
 	  echo "<h1>SUCCESS!</h1>">$VIEWERFILE
@@ -26,10 +29,12 @@ function checkHistory {
 	echo "<h1>Please write the following command: echo \"Hello World!\" $COUNT </h1></p>`history`">$VIEWERFILE
   done 
 }
+
+echo "Inicio"
 echo "<h1>Please write the following command: echo \"Hello World!\"</h1>">$VIEWERFILE
 echo "TECHIO> open -s /project/target/ /viewer.html"
 echo "TECHIO> terminal"
 
 echo "Going to test $TESTING">/tmp/testing
 checkHistory $TESTING
-# }
+
