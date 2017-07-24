8da9d6129a7197c5c31d42569926acaa1541a1ac
Marchete <marchena.carlos@gmail.com>
2017-07-24 12:52:46 -0500

    Testing

--- projects/hello.sh
+++ projects/hello.sh
@@ -2,12 +2,13 @@
 #!/bin/bash
 
 VIEWERFILE="/project/target/viewer.txt"
-
+TESTING='echo "Hello World!"'
 function checkHistory {
   FILE=`history | grep '$1' | grep -v history |wc -l`
+  echo "Status of $FILE ">/tmp/status
   FINISHED=0
   while [ $FINISHED -eq 0 ]; do
-    if [ "$FILE" -ge 1 ]
+    if [ $FILE -ge 1 ]
     then
 	  echo "<h1>SUCCESS!</h1>">$VIEWERFILE
 	  sleep 2s
@@ -17,9 +18,10 @@ function checkHistory {
     sleep 0.1
   done 
 }
-echo "<h1>Please write the following command: echo \"Hello World!\"</h1>">$VIEWERFILE
+echo '<h1>Please write the following command: echo "Hello World!"</h1>'>$VIEWERFILE
 echo "TECHIO> open -s /project/target/ /viewer.html"
 echo "TECHIO> terminal"
 
-checkHistory "echo \"Hello World!\""
+echo 'Going to test $TESTING'>/tmp/testing
+checkHistory $TESTING
 # }
