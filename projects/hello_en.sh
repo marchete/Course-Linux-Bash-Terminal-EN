a3163fcfa6f3ac60d359de65cdb12eae0a532e59
Marchete <marchena.carlos@gmail.com>
2017-07-25 20:23:18 -0500

    Testing

--- projects/hello_en.sh
+++ projects/hello_en.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-HELP[0]="<h2>Please write the following command: <button class=\"btn\">echo "Hello World!"</button></h2> and press Enter</p>"
+HELP[0]="<h2>Please write the following command: <button class=\"btn\">echo \"Hello World!\"</button></h2> and press Enter</p>"
 WELLDONE_WEB="<h2><span class=\"glyphicon glyphicon-ok-circle text-success\"></span> Well Done! Continue to the next lesson.</h2>"
 WELLDONE="Well Done! Continue to the next lesson."
