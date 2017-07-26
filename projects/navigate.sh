8efa9ab9c7a702a4390becca22d3ff6a9c0cb105
Marchete <marchena.carlos@gmail.com>
2017-07-25 20:09:57 -0500

    Testing

--- projects/navigate.sh
+++ projects/navigate.sh
@@ -12,40 +12,18 @@ VIEWERFILE="$WEBROOT/content.html"
 touch /root/.bash_history
 
 #Open server. Magic 1-liner
-#ruby -run -ehttpd $WEBROOT/ -p8000 & &>/dev/nul
 ruby -rwebrick -e"WEBrick::HTTPServer.new(:Port => 8000, :DocumentRoot => \"$WEBROOT\", Logger: WEBrick::Log.new(\"/dev/null\"), AccessLog: [],).start"&
 echo "TECHIO> open --port 8000 /viewer.html"
-
-
-
-function sendMessage {
-echo "$1">$VIEWERFILE
-}
-
-function checkHistory {
-  FIND=$1
-  FINISHED=0
-  while [ $FINISHED -eq 0 ]; do
-    FILE=`cat /root/.bash_history | grep "$FIND" | grep -v /root/.bash_history |wc -l`
-    if [ $FILE -ge 1 ]
-    then
-      FINISHED=1
-	else 
-	  sleep 0.3	
-    fi
-    
-  done 
-}
 
 echo "TECHIO> terminal"
 source navigate_${LOCALE}.sh
 source navigate_CMD.sh
+source functions.sh
 tLen=${#HELP[@]}
 
 for (( i=0; i<${tLen}; i++ ));
 do
   sendMessage "${HELP[$i]}"
-  echo "${CMD[$i]}"
   eval ${CMD[$i]}
 done
 sendMessage "$WELLDONE_WEB"
