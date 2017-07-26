14889072293363f6cac1dbd05e289df17466f680
Marchete <marchena.carlos@gmail.com>
2017-07-26 10:21:38 -0500

    Testing

--- projects/RUN.sh
+++ projects/RUN.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 EXERCISE=$1
-LOCALE=$2
+LOCALE=$2  #en,es,fr, etc...
+#Load functions, initialize web mini-server to write instructions there
 source functions.sh
+
+#Default load English Texts
 source "./exercises/${EXERCISE}/TEXT.sh"
+#If exists a locale file for some language, overwrite English texts
 if [ -e "./exercises/${EXERCISE}/TEXT_${LOCALE}.sh" ]; then source "./exercises/${EXERCISE}/TEXT_${LOCALE}.sh"; fi
+
+#The same for Commands
 source "./exercises/${EXERCISE}/CMD.sh"
+#Commands can be translated too if needed.
 if [ -e "./exercises/${EXERCISE}/CMD_${LOCALE}.sh" ]; then source "./exercises/${EXERCISE}/CMD_${LOCALE}.sh"; fi
 
-tLen=${#HELP[@]}
 
+#Steps of the playground
+tLen=${#HELP[@]}
 for (( i=0; i<${tLen}; i++ ));
 do
   sendMessage "${HELP[$i]}"
   eval ${CMD[$i]}
 done
+#Close playground
 sendMessage "$WELLDONE_WEB"
 echo "$WELLDONE"
-
 sleep 1s	
 echo "TECHIO> success true"
